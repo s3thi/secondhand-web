ac26cfb1b7571cb536cffec504d651b2288e0e95
Ankur Sethi <contact@ankursethi.in>
2013-06-30 20:01:51 +0530

    Added the ApiToken service to manage authentication tokens.

---
+++ app/scripts/services/api_token.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+//
+// The ApiToken service takes care of all our authentication needs. It is
+// responsible for logging in, logging out and managing API tokens in
+// localStorage.
+//
+angular.module('SecondhandApp')
+  .factory('ApiToken', ['$http', '$rootScope', 'Config', function ($http, $rootScope, Config) {
+    var ApiToken = {};
+
+    ApiToken.haveToken = function () {
+      return localStorage.getItem('apiToken') !== null;
+    };
+
+    ApiToken.setAuthorizationHeaders = function(token) {
+      $http.defaults.headers.common['Authorization'] = 'Token ' + token;
+    };
+
+    ApiToken.clearAuthorizationHeaders = function() {
+      $http.defaults.headers.common['Authorization'] = undefined;
+    };
+
+    ApiToken.storeToken = function(data) {
+      localStorage.setItem('apiToken', data.token);
+      ApiToken.setAuthorizationHeaders(data.token);
+    };
+
+    ApiToken.clearToken = function() {
+      localStorage.removeItem('apiToken');
+      ApiToken.clearAuthorizationHeaders();
+    };
+
+    ApiToken.getToken = function() {
+      return localStorage.getItem('apiToken');
+    };
+
+    ApiToken.requestApiToken = function (credentials) {
+      $http.post(Config.BASE_API_URL + 'token', {
+        username: credentials.username,
+        password: credentials.password
+      })
+        .success(function (data, status) {
+          if (status === 201) {
+            ApiToken.storeToken(data);
+            $rootScope.$broadcast('logged_in');
+          }
+        });
+    };
+
+    return ApiToken;
+  }]);
