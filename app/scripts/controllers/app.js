d86e3f226b47d33c85468d3a8ef6890ed1d1c23c
Ankur Sethi <contact@ankursethi.in>
2013-07-08 20:01:07 +0530

    When the user logs in, use $route to re-render the page instead of reloading it.

--- app/scripts/controllers/app.js
+++ app/scripts/controllers/app.js
@@ -4,16 +4,20 @@
 //  AppController is the parent of all controllers in our app.
 //
 angular.module('SecondhandApp')
-  .controller('AppCtrl', function($scope, $window, ApiToken) {
+  .controller('AppCtrl', function($scope, $route, ApiToken) {
     // HACK!
     // Refresh the page when the 'logged_in' event occurs. This event will
     // mostly be broadcast by the ApiToken service in case of a successful
     // login.
     $scope.$on('logged_in', function(event) {
-      $window.location.href = '/';
+      $route.reload();
     });
 
     $scope.haveToken = function() {
       return ApiToken.haveToken();
+    };
+
+    $scope.logout = function() {
+      ApiToken.logout();
     };
   });
