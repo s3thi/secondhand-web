3beb385b1360e742b2d1da93fcb08d6d3f7eaedf
Ankur Sethi <contact@ankursethi.in>
2013-07-09 10:08:44 +0530

    Added MIT license header to all JavaScript files.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,3 +1,10 @@
+/*
+  Copyright (c) 2013 Ankur Sethi <contact@ankursethi.in>
+  Licensed under the terms of the MIT license.
+  See the file LICENSE for copying permissions.
+*/
+
+
 //
 // LoginCtrl is responsible for logging the user in.
 //
--- app/scripts/services/work_session.js
+++ app/scripts/services/work_session.js
@@ -1,3 +1,10 @@
+/*
+  Copyright (c) 2013 Ankur Sethi <contact@ankursethi.in>
+  Licensed under the terms of the MIT license.
+  See the file LICENSE for copying permissions.
+*/
+
+
 'use strict';
 
 //
