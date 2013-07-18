5a50ab7b6e5861d3639d5057fc82a89e740b7545
Ankur Sethi <contact@ankursethi.in>
2013-07-18 12:48:03 +0530

    Format time properly.

---
+++ app/scripts/filters/secondhand_time.js
@@ -0,0 +1,25 @@
+/*
+  Copyright (c) 2013 Ankur Sethi <contact@ankursethi.in>
+  Licensed under the terms of the MIT license.
+  See the file LICENSE for copying permissions.
+*/
+
+
+'use strict';
+
+angular.module('SecondhandApp')
+  .filter('secondhand_time', function() {
+    return function(ms) {
+      ms = parseInt(ms, 10);
+
+      var hours = Math.floor(ms / (60*60*1000));
+      var minutes = Math.floor((ms - hours*60*60*1000) / (1000*60));
+      var seconds = Math.floor((ms - minutes*60*1000) / 1000);
+
+      if (hours   < 10) { hours   = '0' + hours;   }
+      if (minutes < 10) { minutes = '0' + minutes; }
+      if (seconds < 10) { seconds = '0' + seconds; }
+
+      return (hours + ':' + minutes + ':' + seconds);
+    };
+  });
