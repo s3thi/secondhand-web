79eb1b63c356d7002b47c5ef1a9fa3e99e155164
Ankur Sethi <contact@ankursethi.in>
2013-07-19 13:27:50 +0530

    Added a shaLoading directive to display a spinner while a view is loading.

---
+++ app/scripts/directives/loading.js
@@ -0,0 +1,26 @@
+/*
+  Copyright (c) 2013 Ankur Sethi <contact@ankursethi.in>
+  Licensed under the terms of the MIT license.
+  See the file LICENSE for copying permissions.
+*/
+
+
+'use strict';
+
+angular.module('SecondhandApp')
+  .directive('shaLoading', function() {
+    return {
+      restrict: 'A',
+      replace: true,
+      transclude: true,
+      scope: {
+        loading: '=shaLoading'
+      },
+      templateUrl: 'scripts/directives/templates/loading.html',
+      link: function(scope, element, attrs) {
+        var spinner = new Spinner().spin();
+        var loadingContainer = element.find('.sha-loading-spinner-container')[0];
+        loadingContainer.appendChild(spinner.el);
+      }
+    };
+  });
